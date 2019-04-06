f9e1205cc2db7a4a565e87c3063ec9a916e7e4f1
yuanhuihai <yuanhuihai@sina.com>
2019-04-06 15:01:02 +0800

    20190406

--- PLC_DBW_Dynamic_Curve/Form1.Designer.cs
+++ PLC_DBW_Dynamic_Curve/Form1.Designer.cs
@@ -29,12 +29,294 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.connectPlc = new System.Windows.Forms.Button();
+            this.startRead = new System.Windows.Forms.Button();
+            this.dbwValue = new System.Windows.Forms.TextBox();
+            this.dbwNum = new System.Windows.Forms.TextBox();
+            this.dbNum = new System.Windows.Forms.TextBox();
+            this.readCycle = new System.Windows.Forms.TextBox();
+            this.plcSlot = new System.Windows.Forms.TextBox();
+            this.plcRack = new System.Windows.Forms.TextBox();
+            this.plcIp = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.connectStatus = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.TextBox();
+            this.disconnectPlc = new System.Windows.Forms.Button();
+            this.finishRead = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.finishRead);
+            this.groupBox1.Controls.Add(this.disconnectPlc);
+            this.groupBox1.Controls.Add(this.status);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.connectStatus);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.connectPlc);
+            this.groupBox1.Controls.Add(this.startRead);
+            this.groupBox1.Controls.Add(this.dbwValue);
+            this.groupBox1.Controls.Add(this.dbwNum);
+            this.groupBox1.Controls.Add(this.dbNum);
+            this.groupBox1.Controls.Add(this.readCycle);
+            this.groupBox1.Controls.Add(this.plcSlot);
+            this.groupBox1.Controls.Add(this.plcRack);
+            this.groupBox1.Controls.Add(this.plcIp);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(942, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "读取PLC中DBW的值";
+            // 
+            // connectPlc
+            // 
+            this.connectPlc.Location = new System.Drawing.Point(341, 27);
+            this.connectPlc.Name = "connectPlc";
+            this.connectPlc.Size = new System.Drawing.Size(57, 23);
+            this.connectPlc.TabIndex = 8;
+            this.connectPlc.Text = "连接";
+            this.connectPlc.UseVisualStyleBackColor = true;
+            this.connectPlc.Click += new System.EventHandler(this.connectPlc_Click);
+            // 
+            // startRead
+            // 
+            this.startRead.Location = new System.Drawing.Point(341, 68);
+            this.startRead.Name = "startRead";
+            this.startRead.Size = new System.Drawing.Size(57, 23);
+            this.startRead.TabIndex = 7;
+            this.startRead.Text = "读取";
+            this.startRead.UseVisualStyleBackColor = true;
+            this.startRead.Click += new System.EventHandler(this.startRead_Click);
+            // 
+            // dbwValue
+            // 
+            this.dbwValue.Location = new System.Drawing.Point(836, 10);
+            this.dbwValue.Name = "dbwValue";
+            this.dbwValue.ReadOnly = true;
+            this.dbwValue.Size = new System.Drawing.Size(100, 21);
+            this.dbwValue.TabIndex = 6;
+            // 
+            // dbwNum
+            // 
+            this.dbwNum.Location = new System.Drawing.Point(290, 67);
+            this.dbwNum.Name = "dbwNum";
+            this.dbwNum.Size = new System.Drawing.Size(33, 21);
+            this.dbwNum.TabIndex = 5;
+            // 
+            // dbNum
+            // 
+            this.dbNum.Location = new System.Drawing.Point(216, 68);
+            this.dbNum.Name = "dbNum";
+            this.dbNum.Size = new System.Drawing.Size(39, 21);
+            this.dbNum.TabIndex = 4;
+            // 
+            // readCycle
+            // 
+            this.readCycle.Location = new System.Drawing.Point(75, 67);
+            this.readCycle.Name = "readCycle";
+            this.readCycle.Size = new System.Drawing.Size(62, 21);
+            this.readCycle.TabIndex = 3;
+            // 
+            // plcSlot
+            // 
+            this.plcSlot.Location = new System.Drawing.Point(296, 27);
+            this.plcSlot.Name = "plcSlot";
+            this.plcSlot.Size = new System.Drawing.Size(39, 21);
+            this.plcSlot.TabIndex = 2;
+            // 
+            // plcRack
+            // 
+            this.plcRack.Location = new System.Drawing.Point(224, 27);
+            this.plcRack.Name = "plcRack";
+            this.plcRack.Size = new System.Drawing.Size(31, 21);
+            this.plcRack.TabIndex = 1;
+            // 
+            // plcIp
+            // 
+            this.plcIp.Location = new System.Drawing.Point(59, 27);
+            this.plcIp.Name = "plcIp";
+            this.plcIp.Size = new System.Drawing.Size(116, 21);
+            this.plcIp.TabIndex = 0;
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(12, 177);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(942, 240);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "IP地址";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(181, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "机架号";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(261, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "插槽";
+            // 
+            // connectStatus
+            // 
+            this.connectStatus.AutoSize = true;
+            this.connectStatus.Location = new System.Drawing.Point(425, 32);
+            this.connectStatus.Name = "connectStatus";
+            this.connectStatus.Size = new System.Drawing.Size(0, 12);
+            this.connectStatus.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "采样周期";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(149, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "ms";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(181, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "DB块";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(261, 72);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 12);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "DBW";
+            // 
+            // status
+            // 
+            this.status.Location = new System.Drawing.Point(730, 10);
+            this.status.Name = "status";
+            this.status.ReadOnly = true;
+            this.status.Size = new System.Drawing.Size(100, 21);
+            this.status.TabIndex = 17;
+            // 
+            // disconnectPlc
+            // 
+            this.disconnectPlc.Location = new System.Drawing.Point(404, 27);
+            this.disconnectPlc.Name = "disconnectPlc";
+            this.disconnectPlc.Size = new System.Drawing.Size(57, 23);
+            this.disconnectPlc.TabIndex = 18;
+            this.disconnectPlc.Text = "断开";
+            this.disconnectPlc.UseVisualStyleBackColor = true;
+            // 
+            // finishRead
+            // 
+            this.finishRead.Location = new System.Drawing.Point(404, 67);
+            this.finishRead.Name = "finishRead";
+            this.finishRead.Size = new System.Drawing.Size(57, 23);
+            this.finishRead.TabIndex = 19;
+            this.finishRead.Text = "暂停";
+            this.finishRead.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(966, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button startRead;
+        private System.Windows.Forms.TextBox dbwValue;
+        private System.Windows.Forms.TextBox dbwNum;
+        private System.Windows.Forms.TextBox dbNum;
+        private System.Windows.Forms.TextBox readCycle;
+        private System.Windows.Forms.TextBox plcSlot;
+        private System.Windows.Forms.TextBox plcRack;
+        private System.Windows.Forms.TextBox plcIp;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button connectPlc;
+        private System.Windows.Forms.Button finishRead;
+        private System.Windows.Forms.Button disconnectPlc;
+        private System.Windows.Forms.TextBox status;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label connectStatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- PLC_DBW_Dynamic_Curve/Form1.cs
+++ PLC_DBW_Dynamic_Curve/Form1.cs
@@ -1,19 +1,248 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Sharp7;
 
 namespace PLC_DBW_Dynamic_Curve
 {
     public partial class Form1 : Form
     {
+        private S7Client Client;
+        private byte[] Buffer = new byte[65536];
+        private Queue<String> dataQueue = new Queue<String>(100);
+        private int num = 1;//每次添加一个数据
+
         public Form1()
         {
             InitializeComponent();
+            Client = new S7Client();
+            if (IntPtr.Size == 4)
+                this.Text = this.Text + " - Running 32 bit Code";
+            else
+                this.Text = this.Text + " - Running 64 bit Code";
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = System.Convert.ToInt32(readCycle.Text);
+      
+            getDbwValues();
+       
+            UpdateQueueValue();
+            this.chart1.Series[0].Points.Clear();
+            for (int i = 0; i < dataQueue.Count; i++)
+            {
+                this.chart1.Series[0].Points.AddXY((i + 1), dataQueue.ElementAt(i));
+            }
+        }
+
+        // 初始化图表
+        private void InitChart()
+        {
+            //定义图表区域
+            this.chart1.ChartAreas.Clear();
+            ChartArea chartArea1 = new ChartArea("C1");
+            this.chart1.ChartAreas.Add(chartArea1);
+            //定义存储和显示点的容器
+            this.chart1.Series.Clear();
+            Series series1 = new Series("DBW");
+            series1.ChartArea = "C1";
+            this.chart1.Series.Add(series1);
+            //设置图表显示样式
+            this.chart1.ChartAreas[0].AxisY.Minimum = 800;
+            this.chart1.ChartAreas[0].AxisY.Maximum = 1000;
+            this.chart1.ChartAreas[0].AxisX.Interval = 5;
+            this.chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            this.chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            //设置标题
+            this.chart1.Titles.Clear();
+            this.chart1.Titles.Add("S01");
+            this.chart1.Titles[0].Text = "DBW的值变化曲线";
+            this.chart1.Titles[0].ForeColor = Color.RoyalBlue;
+            this.chart1.Titles[0].Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            //设置图表显示样式
+            this.chart1.Series[0].Color = Color.Red;
+        
+                this.chart1.Titles[0].Text = string.Format("{0}", "DBW的值变化曲线");
+                this.chart1.Series[0].ChartType = SeriesChartType.Line;
+           
+          
+            this.chart1.Series[0].Points.Clear();
+        }
+
+        //更新队列中的值
+        private void UpdateQueueValue()
+        {
+            if (dataQueue.Count > 100)
+            {
+                //先出列
+                for (int i = 0; i < num; i++)
+                {
+                    dataQueue.Dequeue();
+                }
+            }
+        
+             
+                for (int i = 0; i < num; i++)
+                {
+                    dataQueue.Enqueue(dbwValue.Text.ToString());
+                }
+            }
+
+        private void connectPlc_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(plcIp.Text))
+            {
+               
+                plcIp.Focus();
+            }
+            if (string.IsNullOrEmpty(plcRack.Text))
+            {
+               
+                plcRack.Focus();
+            }
+            if (string.IsNullOrEmpty(plcSlot.Text))
+            {
+               
+                plcSlot.Focus();
+            }
+            else
+            {
+                int Result;
+                int Rack = System.Convert.ToInt32(plcRack.Text);
+                int Slot = System.Convert.ToInt32(plcSlot.Text);
+                Result = Client.ConnectTo(plcIp.Text, Rack, Slot);
+
+                if (Result == 0)
+                {
+
+                    plcIp.Enabled = false;
+                    plcRack.Enabled = false;
+                    plcSlot.Enabled = false;
+                    connectPlc.Enabled = false;
+
+                }
+            }
+        }
+
+        private void startRead_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(readCycle.Text))
+            {
+               
+                readCycle.Focus();
+            }
+            if (string.IsNullOrEmpty(dbNum.Text))
+            {
+               
+                dbNum.Focus();
+            }
+            if (string.IsNullOrEmpty(dbwNum.Text))
+            {
+                dbwNum.Focus();
+            }
+           
+            else
+            {
+                timer1.Start();
+                dbNum.Enabled = false;
+                dbwNum.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitChart();
+        }
+
+        private void ReadDbw()
+        {
+            int DBNumber;
+            int Size = 10;
+            int Result;
+
+            DBNumber = System.Convert.ToInt32(dbNum.Text);
+            Result = Client.DBRead(DBNumber, 0, Size, Buffer);
+            if (Result == 0)
+                HexDump(Buffer, Size);
+
+        }
+        private void HexDump(byte[] bytes, int Size)
+        {
+            if (bytes == null)
+                return;
+            int bytesLength = Size;
+            int bytesPerLine = 16;
+
+            char[] HexChars = "0123456789ABCDEF".ToCharArray();
+
+            int firstHexColumn =
+                  8                   // 8 characters for the address
+                + 3;                  // 3 spaces
+
+            int firstCharColumn = firstHexColumn
+                + bytesPerLine * 3       // - 2 digit for the hexadecimal value and 1 space
+                + (bytesPerLine - 1) / 8 // - 1 extra space every 8 characters from the 9th
+                + 2;                  // 2 spaces 
+
+            int lineLength = firstCharColumn
+                + bytesPerLine           // - characters to show the ascii value
+                + Environment.NewLine.Length; // Carriage return and line feed (should normally be 2)
+
+            char[] line = (new String(' ', lineLength - 2) + Environment.NewLine).ToCharArray();
+            int expectedLines = (bytesLength + bytesPerLine - 1) / bytesPerLine;
+            StringBuilder result = new StringBuilder(expectedLines * lineLength);
+
+            for (int i = 0; i < bytesLength; i += bytesPerLine)
+            {
+                line[0] = HexChars[(i >> 28) & 0xF];
+                line[1] = HexChars[(i >> 24) & 0xF];
+                line[2] = HexChars[(i >> 20) & 0xF];
+                line[3] = HexChars[(i >> 16) & 0xF];
+                line[4] = HexChars[(i >> 12) & 0xF];
+                line[5] = HexChars[(i >> 8) & 0xF];
+                line[6] = HexChars[(i >> 4) & 0xF];
+                line[7] = HexChars[(i >> 0) & 0xF];
+
+                int hexColumn = firstHexColumn;
+                int charColumn = firstCharColumn;
+
+                for (int j = 0; j < bytesPerLine; j++)
+                {
+                    if (j > 0 && (j & 7) == 0) hexColumn++;
+                    if (i + j >= bytesLength)
+                    {
+                        line[hexColumn] = ' ';
+                        line[hexColumn + 1] = ' ';
+                        line[charColumn] = ' ';
+                    }
+                    else
+                    {
+                        byte b = bytes[i + j];
+                        line[hexColumn] = HexChars[(b >> 4) & 0xF];
+                        line[hexColumn + 1] = HexChars[b & 0xF];
+                        line[charColumn] = (b < 32 ? '·' : (char)b);
+                    }
+                    hexColumn += 3;
+                    charColumn++;
+                }
+                result.Append(line);
+            }
+
+        }
+        private void getDbwValues()
+        {
+
+
+            ReadDbw();
+            int Pos = System.Convert.ToInt32(dbwNum.Text);
+            int S7Int = S7.GetIntAt(Buffer, Pos);
+            dbwValue.Text = System.Convert.ToString(S7Int);
+
         }
     }
-}
+    }
