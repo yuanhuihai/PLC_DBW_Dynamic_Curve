c5b95e1fab06b8ce6e9d4fc575c5496b6959678b
yuanhuihai <yuanhuihai@sina.com>
2019-12-02 12:23:48 +0800

    优化读取DBRead(db,dbw,2,buffer)。实现读取任意DBW的值

--- PLC_DBW_Dynamic_Curve/Form1.cs
+++ PLC_DBW_Dynamic_Curve/Form1.cs
@@ -161,78 +161,15 @@ namespace PLC_DBW_Dynamic_Curve
         private void ReadDbw()
         {
             int DBNumber;
-            int Size = 10;
+            int Start = Convert.ToInt16(dbwNum.Text);
             int Result;
 
             DBNumber = System.Convert.ToInt32(dbNum.Text);
-            Result = Client.DBRead(DBNumber, 0, Size, Buffer);
-            if (Result == 0)
-                HexDump(Buffer, Size);
+            Result = Client.DBRead(DBNumber, Start, 2, Buffer);
+          
 
         }
-        private void HexDump(byte[] bytes, int Size)
-        {
-            if (bytes == null)
-                return;
-            int bytesLength = Size;
-            int bytesPerLine = 16;
 
-            char[] HexChars = "0123456789ABCDEF".ToCharArray();
-
-            int firstHexColumn =
-                  8                   // 8 characters for the address
-                + 3;                  // 3 spaces
-
-            int firstCharColumn = firstHexColumn
-                + bytesPerLine * 3       // - 2 digit for the hexadecimal value and 1 space
-                + (bytesPerLine - 1) / 8 // - 1 extra space every 8 characters from the 9th
-                + 2;                  // 2 spaces 
-
-            int lineLength = firstCharColumn
-                + bytesPerLine           // - characters to show the ascii value
-                + Environment.NewLine.Length; // Carriage return and line feed (should normally be 2)
-
-            char[] line = (new String(' ', lineLength - 2) + Environment.NewLine).ToCharArray();
-            int expectedLines = (bytesLength + bytesPerLine - 1) / bytesPerLine;
-            StringBuilder result = new StringBuilder(expectedLines * lineLength);
-
-            for (int i = 0; i < bytesLength; i += bytesPerLine)
-            {
-                line[0] = HexChars[(i >> 28) & 0xF];
-                line[1] = HexChars[(i >> 24) & 0xF];
-                line[2] = HexChars[(i >> 20) & 0xF];
-                line[3] = HexChars[(i >> 16) & 0xF];
-                line[4] = HexChars[(i >> 12) & 0xF];
-                line[5] = HexChars[(i >> 8) & 0xF];
-                line[6] = HexChars[(i >> 4) & 0xF];
-                line[7] = HexChars[(i >> 0) & 0xF];
-
-                int hexColumn = firstHexColumn;
-                int charColumn = firstCharColumn;
-
-                for (int j = 0; j < bytesPerLine; j++)
-                {
-                    if (j > 0 && (j & 7) == 0) hexColumn++;
-                    if (i + j >= bytesLength)
-                    {
-                        line[hexColumn] = ' ';
-                        line[hexColumn + 1] = ' ';
-                        line[charColumn] = ' ';
-                    }
-                    else
-                    {
-                        byte b = bytes[i + j];
-                        line[hexColumn] = HexChars[(b >> 4) & 0xF];
-                        line[hexColumn + 1] = HexChars[b & 0xF];
-                        line[charColumn] = (b < 32 ? '·' : (char)b);
-                    }
-                    hexColumn += 3;
-                    charColumn++;
-                }
-                result.Append(line);
-            }
-
-        }
         private void getDbwValues()
         {
 
